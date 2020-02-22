da93414e2ca12c6ac98c953fd95ab762b97d566a
idoko <radpangolin@gmail.com>
2020-02-22 14:12:10 +0100

    setup event and listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,8 +15,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        'App\Events\RecipeEvent' => [
+            'App\Listeners\BroadcastNotification'
         ],
     ];
 
